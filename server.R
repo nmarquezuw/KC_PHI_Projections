6b376ea6ddb06e5ef9599140d0532f7d8881e27a
Steven Bao <whichsteven297@gmail.com>
2020-08-18 21:11:41 -0700

    update website

--- server.R
+++ server.R
@@ -690,4 +690,7 @@ server <- function(input, output, session) {
     clicked_tract$df <- NULL
   })
 
+  outputOptions(output, "map", suspendWhenHidden = FALSE)
+  outputOptions(output, "plot", suspendWhenHidden = FALSE)
+
 }
