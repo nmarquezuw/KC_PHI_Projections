a870704fc3a6316ea5d9def7e6bc0069fd5b2783
Steven Bao <whichsteven297@gmail.com>
2020-08-18 21:51:16 -0700

    update website

--- ui.R
+++ ui.R
@@ -40,7 +40,7 @@ radioTooltip <- function(id, choice, title, placement = "bottom", trigger = "hov
 #------------shiny ui--------
 ui <- dashboardPage(
   dashboardHeader(title = "PHI INTERACTIVE VIZ"),
-
+  
   dashboardSidebar(
     sidebarMenu(
       menuItem(
@@ -63,13 +63,13 @@ ui <- dashboardPage(
     img(
       src = "uw_logo.png",
       height = "45px",
-      style = "position: absolute; left: 10px; bottom: 13px;"
+      style = "position: fixed; left: 10px; bottom: 13px;"
     ),
 
     img(
       src = "csde_logo.gif",
       height = "50px",
-      style = "position: absolute; left: 100px; bottom: 10px;"
+      style = "position: fixed; left: 100px; bottom: 10px;"
     ),
     collapsed = FALSE
   ),
@@ -79,10 +79,50 @@ ui <- dashboardPage(
       tabItem(
         tabName = "intro",
 
-        h1(
-          HTML("Welcome to the Interactive Visualization Tool<br/>For UW’s 2020 Population Health Applied Research Project"),
-          style = "text-align: center;"
-        )
+        tags$style(type='text/css', "p {font-size: 15px !important} "),
+
+
+        h1("King County Population Projections Overview"),
+        p("With support from the Public Health Services Division of Seattle & King County and in partnership with the University of Washington’s Center for Studies in Demography and Ecology, the 2020 Population Health Initiative Applied Research Fellowship Program developed small area population projections at the Census tract and Health Reporting Area (HRA) levels for King County by age, race and ethnicity in 5 year intervals from 2020 to 2045. These projections use demographic data from the American Community Survey and the Decennial Census, along with existing population projections from the Washington Office of Financial Management (OFM) and Puget Sound Regional Council (PSRC)."),
+        br(),
+
+        h1("User Guide"),
+        p("The interactive mapping tool can be used to explore the changing dynamics of King County’s population over time by age, race and ethnicity, within census tracts and health reporting areas. For example, the [name of tool] let’s you see how King County’s population is expected to grow until 2045 if current trends in fertility, mortality, and migration continue. To explore the [name of tool], click the Interactive Map tab on the left side of this page."),
+        br(),
+
+        h3(tags$i("Measure")),
+        p("To begin, select the measure by which you want to view the population totals by tract. Selecting the count measure will provide a projected population count within each census tract. This population count will be based on the selection of year, sex, age, and race groups chosen, as described below. "),
+        p("Selecting the percentage measure will provide the percentage of the selected group’s population divided by the total population of the corresponding geography. Note the legend icon in the bottom right corner of the map matches your selection accordingly, as does the y-axis of the line graph."),
+        br(),
+
+        h3(tags$i("Year and Sex")),
+        p("To display projections by year, use the drop down menu to select the year of interest. To explore projections by sex choose the female, male or both option."),
+        br(),
+
+        h3(tags$i("Age")),
+        p("The age feature allows users to display projections by specific age ranges in 5 year increments. Use the toggle feature to set a minimum and maximum age range (0 to 85+), or click the select all option to display projections across all ages. "),
+        br(),
+
+        h3(tags$i("Race and Ethnicity")),
+        p("Projections can be displayed by specific race and ethnic groups by selecting from the provided race and ethnic categories. Selecting the all option provides projections across all races and ethnicities."),
+        br(),
+
+        h3(tags$i("Line Graph")),
+        p("The line graph feature provides an additional visual for understanding how different race and ethnic groups are changing over time. You may select multiple race and ethnic groups to view at once. Be sure to deselect when you no longer want to view a particular group in the line graph visual. "),
+        br(),
+
+        h3(tags$i("Map")),
+        p("All of the above selections are reflected in the map feature. Click on the map for detailed information by census tract. Additionally, the layers icon in the top right corner of the map allows the user to overlay multiple, current county facilities across all projections over time. "),
+        br(),
+
+        h2("Assumptions"),
+        p("Although population projections aim to provide important estimates for planners, service providers, researchers, and the general public, they are only a reflection of what the population could look like if current population trends continue. They are not determinative of the future. . Further, small area projections can be more uncertain and so less predictive than projections for larger areas and populations, and less accurate for the distant future. Like all forecasts, our projections reflect a number of assumptions about expected populations dynamics in King County over the forecast period."),
+        br(),
+
+        h2("Methods"),
+        p("The projections presented here use the Hamilton Perry method with smoothing (a variant of the cohort-component method) based on OFM’s 5-year age data by race, ethnicity, and sex at state, county, & tract level from [dates] as observed in data from the [date] American Community Survey and 2010 decennial census data. Projections were compared with OFM state-level population projections by sex, age, race and ethnicity from 2020 to 2045 and OFM county-level population projections by sex and age from 2020 to 2045."),
+        br()
+
       ),
 
 
@@ -189,7 +229,7 @@ ui <- dashboardPage(
 
               radioButtons(
                 inputId = "race",
-                label = "Race/Ethnicity",
+                label = "Race and Ethnicity",
                 choices = c("All", "AIAN", "Asian", "Black", "Hispanic", "NHOPI", "Two or More Races", "White"),
                 selected = "All"
               ),
